9cc0e2b4a00eecdcaa8b6bda0835f2d28e1f447e
hengquxiaoxiong <56551090+hengquxiaoxiong@users.noreply.github.com>
2020-03-26 17:12:15 +0800

    Add files via upload

---
+++ 2 puestion.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float r5,r3,r2,r1,r0,p,p1,p2,p3,p4,p5;
+	p=1000;
+	r5=0.03;
+	r3=0.0275;
+	r2=0.021;
+	r1=0.015;
+	r0=0.0035;
+	p1=p*((1+r5)*5);
+	p2=p*(1+2*r2)*(1+3*r3);
+	p3=p*(1+3*r3)*(1+2*r2);
+	p4=p*pow(1+r1,5);
+	p5=p*pow(1+r0/4,4*5);
+	printf("p1=%f\n",p1);
+	printf("p2=%f\n",p2);
+	printf("p3=%f\n",p2);
+	printf("p4=%f\n",p4);
+	printf("p5=%f\n",p5);
+	return 0;
+	}
